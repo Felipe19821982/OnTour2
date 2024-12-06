7c2ca068732f3f277c489d64e20af441434628ee
Felipe19821982 <felipe.arq.10@gmail.com>
2024-12-06 15:49:39 -0300

    Se esta ajustado el formulario de registro para incluir un listado de cursos

--- demo/models.py
+++ demo/models.py
@@ -62,14 +62,20 @@ class Curso(models.Model):
 
 from django.db import models
 from django.contrib.auth.models import User
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
 class Alumno(models.Model):
     nombre = models.CharField(max_length=100)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE, related_name="alumnos")
     apoderado = models.ForeignKey(User, on_delete=models.CASCADE, related_name="alumnos")
 
     def __str__(self):
-        return f"{self.nombre} ({self.curso.nombre})"
-    
+        return f"{self.nombre} - {self.curso.nombre}"
 
 
 
